d28b348ef714f37e707fabba8b2d4b06d62d2346
Ethan Armstrong <w.ethan.armstrong@gmail.com>
2022-05-22 18:09:31 -0700

    added alternative image search functionality

--- compileImages.py
+++ compileImages.py
@@ -1,5 +1,6 @@
 import requests
 import re
+from serpapi import GoogleSearch
 
 
 def normalize_html(s):
@@ -31,29 +32,62 @@ def parse_image_html(s):
     return "https:" + match.group(1)
 
 
-WIKI_URL = "https://en.wikipedia.org/wiki/List_of_fractals_by_Hausdorff_dimension"
-
-res = requests.get(WIKI_URL)
-
-if res.status_code == 200:
-    html = res.text
-
-    match = re.findall(r'<tr>\n[\s\S\n]*?<\/td>\n.*?>(.*)<\/td>\n<td>(.*)<.*\n(.*)', html)
-
-    fractals = []
-    for i in range(len(match)):
-        m = match[i]
-
-        power = normalize_float(m[0])
-
-        if power is not None:
-            name = normalize_html(m[1])
-            image = parse_image_html(m[2])
-            fractals.append((name, power, image))
+def get_alternative_images(name):
+    name += " HD fractal"
 
 
+    print("="*20)
+    print(name)
+    print("="*20)
 
-    for fractal in fractals:
-        print(fractal)
+    params = {
+    "engine": "google",
+    "ijn": "0",
+    "q": name,
+    "google_domain": "google.com",
+    "tbm": "isch",
+    "api_key": "d3c721a310e66cd91398fcad90be734f2158c8f9d0d4fe0761a9671782131124"
+    }
+
+    search = GoogleSearch(params)
+    results = search.get_dict()
+
+    #print(results["images_results"][::8])
+    return [image["original"] for image in results["images_results"][:8] if "original" in image]
 
 
+def main():
+    res = requests.get(WIKI_URL)
+
+    if res.status_code == 200:
+        html = res.text
+
+        match = re.findall(r'<tr>\n[\s\S\n]*?<\/td>\n.*?>(.*)<\/td>\n<td>(.*)<.*\n(.*)', html)
+
+        fractals = []
+        for i in range(len(match)):
+            m = match[i]
+
+            power = normalize_float(m[0])
+
+            if power is not None:
+                name = normalize_html(m[1])
+                image = parse_image_html(m[2])
+                fractals.append((name, power, image))
+
+
+        with open("results.txt", "w", encoding="utf-8") as f:
+            for fractal in fractals:
+                    print(fractal)
+                    f.write(str(fractal))
+                    f.write("\n")
+
+                    alts = get_alternative_images(fractal[0])
+                    for alt in alts:
+                        f.write("\t")
+                        f.write(alt)
+                        f.write("\n")
+
+if "__main__" in __name__:
+    WIKI_URL = "https://en.wikipedia.org/wiki/List_of_fractals_by_Hausdorff_dimension"
+    main()
